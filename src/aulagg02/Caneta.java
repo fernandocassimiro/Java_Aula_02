5874c849542b8b738d8e884904fc31707ff1af86
Fernando Cassimiro de Macedo <fernandocassimiro@uol.com.br>
2018-07-26 10:04:33 -0300

    Java_aula_02

---
+++ src/aulagg02/Caneta.java
@@ -0,0 +1,18 @@
+package aulagg02;
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    void status() {
+        System.out.println ("Uma caneta " + this.cor);
+        System.out.println ("esta tampada? " + this.tampada);
+    }
+    void rabiscar () {
+}
+    void tampar () {
+}
+    void destampar () {
+}    
+    }
